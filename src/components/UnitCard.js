364129eb86c874d7f47b13a9ab4e954616123fcb
lawrie-sm <alertrelic@gmail.com>
2017-11-25 16:45:50 +0000

    Added ingame armour/quality descriptors

--- src/components/UnitCard.js
+++ src/components/UnitCard.js
@@ -5,12 +5,49 @@ class UnitCard extends Component {
   render() {
     const u = this.props.unit;
 
-    // Quality is average of Elan and XP
-    const quality = (parseInt(u.Experience) + parseInt(u.Elan)) / 2;
+
 
     //View flags determines if a unit can hide on the edge of woods (1 = yes 2 = no)
-    let isStealthy = (u.ViewFlags === "1") ? "\u2714" : "\u2718";
+    const isStealthy = (u.ViewFlags === "1") ? "\u2714" : "\u2718";
 
+    const armorVal = parseInt(u.BodyArmour);
+    let armourDesc = "None";
+    if (armorVal > 0) {
+      armourDesc = "Lightly Protected";
+      if (armorVal >= 50) {
+        armourDesc = "Protected";
+        if (armorVal > 50) {
+          armourDesc = "Some Armour";
+          if (armorVal > 75) {
+            armourDesc = "Armoured";
+            if (armorVal > 100) {
+              armourDesc = "Fully Armoured";
+            }
+          }
+        }
+      }
+    } 
+
+    // Quality is average of Elan and XP
+    const qualityVal = (parseInt(u.Experience) + parseInt(u.Elan)) / 2;
+    let qualityDesc = "Untrained";
+    if (qualityVal > 0) {
+      qualityDesc = "Raw";
+      if (qualityVal > 50) {
+        qualityDesc = "Below Average";
+        if (qualityVal > 75) {
+          qualityDesc = "Average";
+          if (qualityVal >= 200) {
+            qualityDesc = "Superior";
+            if (qualityVal === 300) {
+              qualityDesc = "Elite";
+            }
+          } 
+        }
+      }
+    }
+
+    
     return (
       <div className="UnitCard row col-md-3">
         <div className="card">
@@ -21,13 +58,13 @@ class UnitCard extends Component {
           <div className="section section--body">
             <ul>
               <li>
-                <strong>AP: </strong>{u.AP}
+                <strong>Action Points: </strong>{u.AP}
               </li>
               <li>
-                <strong>Armour: </strong>{u.BodyArmour}
+                <strong>Armour: </strong> {armourDesc} ({armorVal})
               </li>
               <li>
-                <strong>Quality </strong>{quality}
+                <strong>Quality: </strong>{qualityDesc} ({qualityVal})
               </li>
               <li>
                 <strong>Men: </strong>{u.TotalMen}
