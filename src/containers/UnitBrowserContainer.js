08de82286f17a978aa815557a76e264a10cc1419
lawrie-sm <alertrelic@gmail.com>
2017-11-29 09:58:29 +0000

    moved onclick to container

--- src/containers/UnitBrowserContainer.js
+++ src/containers/UnitBrowserContainer.js
@@ -2,29 +2,22 @@ import React, { Component } from 'react';
 import UnitTable from '../components/UnitTable';
 import UnitData from '../data/UnitData';
 
-function getClickHandler() {
-  return (
-  function handleClick(state, rowInfo, column, instance) {
-    return (e, handleOriginal) => {
-      console.log(rowInfo)
-      if (handleOriginal) {
-        handleOriginal();
-      }
+class UnitBrowserContainer extends Component {
+
+handleClick = (state, rowInfo, column, instance)  => {
+  return (e, handleOriginal) => {
+    console.log(rowInfo)
+    if (handleOriginal) {
+      handleOriginal();
     }
   }
-  );
 }
 
-
-class UnitBrowserContainer extends Component {
-  
   render() {
     return (
       <div className='UnitBrowserContainer'>
           <UnitTable units={UnitData}
-          onClick={
-            getClickHandler()
-            }/>
+          onClick={this.handleClick}/>
       </div>
     );
   }
