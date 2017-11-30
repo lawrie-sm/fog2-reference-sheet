667fe32e36ac852f6fbd4acfb7a4b6a2847c3547
lawrie-sm <alertrelic@gmail.com>
2017-11-30 09:58:33 +0000

    more changes to rules

--- src/components/UnitViewer.js
+++ src/components/UnitViewer.js
@@ -8,7 +8,7 @@ const UnitViewer = ({selectedUnit, descs}) => {
     return (
     <div className='UnitViewer'>
       <div className='container'>
-        <h4>{selectedUnit.Name} - {selectedUnit.Type}</h4>
+        <h4>{selectedUnit.Name}</h4>
       <div className='row'>
         <div className='twelve columns'>
         <UnitDescriptions unit={selectedUnit} descs={descs}/>
@@ -25,55 +25,68 @@ const UnitViewer = ({selectedUnit, descs}) => {
 };
 
 const UnitDescriptions = ({unit, descs}) => {
+ if(unit && descs) {
 
-  if (unit && descs) {
-    let rules = [descs.find((r) => r.name === unit.Type)];
-    unit.traits.forEach((trait) => {
-      rules.push(descs.find((r) => r.name === trait.name)); //TODO: Account for mixed units (e.g. append '(33%)' to desc?)
-    });
-    
-    let cohesionRules = [];
-    let impactRules = [];
-    let meleeRules = [];
-    let shootingRules = [];
-    let terrainRules = [];
-    let otherRules = [];
+  let unitRuleNames = [unit.Type];
+  unit.traits.forEach ((trait) => unitRuleNames.push(trait.name));
 
-    rules.forEach((r) => {
-      if (r) {
-        if (r.cohesion) {
-          cohesionRules.push(r.cohesion);
-        }
-        if (r.impact) {
-          impactRules.push(r.impact);
-        }
-        if (r.melee) {
-          meleeRules.push(r.melee);
-        }
-        if (r.shooting) {
-          shootingRules.push(r.shooting);
-        }
-        if (r.terrain) {
-          terrainRules.push(r.terrain);
-        }
-        if (r.other) {
-          otherRules.push(r.other);
-        }
-      } else return (<div>Problem getting rules.</div>);
-    });
+  let cohesionRules = getRulesByCategory('cohesion', unitRuleNames, descs);
+  let impactRules = getRulesByCategory('impact', unitRuleNames, descs);
+  let meleeRules = getRulesByCategory('melee', unitRuleNames, descs);
+  let shootingRules = getRulesByCategory('shooting', unitRuleNames, descs);
+  let terrainRules = getRulesByCategory('terrain', unitRuleNames, descs);
+  let otherRules = getRulesByCategory('other', unitRuleNames, descs);
 
     return (
       <ul>
-        <li><strong>Cohesion:</strong> {(cohesionRules.length > 0) ? (cohesionRules.join(' ')) : '' }</li>
-        <li><strong>Impact:</strong> {(impactRules.length > 0) ? impactRules.join(' ') : '' }</li>
-        <li><strong>Melee:</strong> {(meleeRules.length > 0) ? meleeRules.join(' ') : '' }</li>
-        <li><strong>Shooting:</strong> {(shootingRules.length > 0) ? shootingRules.join(' ') : '' }</li>
-        <li><strong>Terrain:</strong> {(terrainRules.length > 0) ? terrainRules.join(' ') : '' }</li>
-        <li><strong>Other:</strong> {(otherRules.length > 0) ? otherRules.join(' ') : '' }</li>
+        <Rules ruleName='Cohesion' rules={cohesionRules} />
+        <Rules ruleName='Impact' rules={impactRules} />
+        <Rules ruleName='Melee' rules={meleeRules} />
+        <Rules ruleName='Shooting' rules={shootingRules} />
+        <Rules ruleName='Terrain' rules={terrainRules} />
+        <Rules ruleName='Other' rules={otherRules} />
       </ul>
     );
 
   } else return (<p>Type not found.</p>);
 };
+
+function getRulesByCategory(category, unitRuleNames, descs) {
+let rules = [];
+unitRuleNames.forEach((ruleName)=> {
+  let rule = descs.find((desc) => desc.name === ruleName);
+  if (rule && rule[category]) {
+    rule[category].forEach((rDesc) => {
+      rules.push(rDesc);
+    });
+  }
+});
+return rules;
+}
+
+const Rules = ({ruleName, rules}) => {
+
+  console.log(rules);
+
+  if (rules && rules.length > 0) {
+    if (rules.length === 1) {
+      return (
+        <div className={ruleName}>
+          <p><strong>{ruleName}:</strong> {rules[0]}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className={ruleName}>
+          <p><strong>{ruleName}:</strong></p>
+          <ul>
+            {rules.map((rule, i) => <li key={i}>{rule}</li>)}
+          </ul>
+        </div>
+      );
+    }
+  } else return '';
+};
+
 
 export default UnitViewer;
--- src/data/Descriptions.js
+++ src/data/Descriptions.js
@@ -12,19 +12,19 @@
 const Descriptions = [
   { 
     name: 'Heavy Foot',
-    cohesion: '+1 to cohesion tests. Opposing non-heavy infantry take -1 modifier if they lose the combat in open terrain.',
-    terrain: 'Severly disordered in difficult terrain. Disordered in rough terrain.',
-    other: ['test other rule', 'another test other']
+    cohesion: ['+1 to cohesion tests.', 'Opposing non-heavy infantry take -1 modifier if they lose the combat in open terrain.'],
+    terrain: ['Severly disordered in difficult terrain. Disordered in rough terrain.'],
+    other: ['heavy test other 1', 'heavy test other 2']
   },
   { 
     name: 'Offensive Spearmen',
-    impact: 'impact test',
-    melee: 'melee test',
-    terrain: 'Terrain test',
+    impact: ['spear impact test'],
+    melee: ['spear melee test'],
+    terrain: ['spear Terrain test'],
   },
   { 
     name: 'Bow',
-    shooting: 'Test Shooting',
+    shooting: ['Bow Test Shooting'],
   }
 ];
 export default Descriptions;
