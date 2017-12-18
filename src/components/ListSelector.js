22a0a5e5fbbb2c48973bafd6f9e1b7055f028fd2
lawrie-sm <alertrelic@gmail.com>
2017-12-18 12:20:01 +0000

    Army list selector added and working

--- src/components/ListSelector.js
+++ src/components/ListSelector.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/components/ListSelector.css';
 
-const ListSelector = ({list}) => {
-  console.log(list);
-  return (
-    <div className='ListSelector'>
-      <span>
-        Army List
-      </span>
-      <select>
-        <option value="test1">Test 1</option>
-        <option value="test2">Test 2</option>
-      </select>
-    </div>
-  );
+class ListSelector extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.props.updateList(event.target.value);
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor='listInput'> Army List </label>
+        <select id='listInput' value={this.state.value} onChange={this.handleChange}>
+        {ArmyLists(this.props.lists)}
+        </select>
+      </form>
+    );
+  }
+
+};
+
+const ArmyLists = (lists) => {
+  const defaultOption = <option key='-1' value='ALL'>(Show All Units)</option>;
+  let listNames = lists.map((list, i) => {
+    const startDate = (list.startDate < 0) ? `${-list.startDate}BC` : `${list.startDate}AD`;
+    const endDate = (list.endDate < 0) ? `${-list.endDate}BC` : `${list.endDate}AD`;
+    return (
+      <option key ={i} value={list.identifier}>
+        {`${list.name} ${startDate}-${endDate}`}
+      </option>
+    );
+  });
+  
+  listNames.sort((a, b) => {
+    if (a.props.children > b.props.children) return 1;
+    if (a.props.children < b.props.children) return -1
+    return 0
+  });
+
+  listNames.unshift(defaultOption);
+  return (listNames);
 };
 
 export default ListSelector;
--- src/containers/UnitBrowserContainer.js
+++ src/containers/UnitBrowserContainer.js
@@ -9,7 +9,7 @@ class UnitBrowserContainer extends Component {
 
   constructor() {
   super();
-    this.state = {selectedUnit: undefined};
+    this.state = {unitData: UnitData, selectedUnit: undefined};
   }
 
   handleClick = (state, rowInfo, column, instance) => {
@@ -27,15 +27,36 @@ class UnitBrowserContainer extends Component {
     }
   }
 
+  updateList = (identifier) => {
+    this.setState({
+      unitData: getUnitDataFromList(identifier, ListData, UnitData),
+      selectedUnit: undefined
+    })
+  }
+
   render() {
     return (
       <div className='UnitBrowserContainer'>
-          <ListSelector list={ListData} />
-          <UnitTable units={UnitData} handleClick={this.handleClick} />
+          <ListSelector lists={ListData} updateList={this.updateList} />
+          <UnitTable units={this.state.unitData} handleClick={this.handleClick} />
           <UnitViewer selectedUnit={this.state.selectedUnit} />
       </div>
     );
   }
 }
+
+const getUnitDataFromList = (identifier, listData, unitData) => {
+  if (identifier === 'ALL') return unitData;
+  const list = listData.find((l) => l.identifier === identifier);
+  const units = list.units;
+  const newUnitData = [];
+  units.forEach(unit => {
+    let thisUnitData = unitData.find((u) => u.Name.toLowerCase() === unit.name.toLowerCase());
+    if (thisUnitData) newUnitData.push(thisUnitData);
+  });
+  return(newUnitData);
+}
+
+//TODO: Add upper and lower limits to table
 
 export default UnitBrowserContainer;
