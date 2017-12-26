70f50187834391a927540ea4210729a7f84b1f48
lawrie-sm <alertrelic@gmail.com>
2017-12-26 16:48:10 +0000

    Reworked modals to ensure only backdrop clicks triggered a close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,14 +7,16 @@ class Modal extends Component {
       return null;
     }
     return (
-      <div className='modal-backdrop' onClick={this.props.onClose}>
+      <div>
+        <div className='modal-backdrop' onClick={this.props.onClose}></div>
         <div className='modal-container'>
-        <button onClick={this.props.onClose}>x</button>
-          <div className='modal-content'>
-            {this.props.children}
-          </div>
+            <button onClick={this.props.onClose}>x</button>
+            <div className='modal-content'>
+              {this.props.children}
+            </div>
         </div>
       </div>
+
     );
   }
 }
